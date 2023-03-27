86cbc19f5bbc664223a87b5c1b5ac684b3de047d
ShapatinAA <113165107+ShapatinAA@users.noreply.github.com>
2023-03-27 10:09:36 +0700

    Update hello2.c

--- lab2/task1/hello2.c
+++ lab2/task1/hello2.c
@@ -1,10 +1,24 @@
 #include <unistd.h>
-#include <sys/syscall.h>
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+#define STD_OUT 1
+#define BUF_SIZE 13
+#define ERR_STATUS -1
 
 int main() {
 
-    write(1, "Hello world!\n", 13);
-    syscall(SYS_write, 1, "Hello my world!\n", 16);
+    sszie_t status;
 
-    return 0;
+    status = write(STD_OUT, "Hello world!\n", BUF_SIZE);
+
+    if (status == ERR_STATUS) {
+
+        printf("ERROR OCCURED");
+        return EXIT_FAILURE;
+
+    }
+
+    return EXIT_SUCCESS;
 }
