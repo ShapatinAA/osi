67a9cfc631e08171d3b72cd6bdbcdf3460732cc4
ShapatinAA <113165107+ShapatinAA@users.noreply.github.com>
2023-03-30 03:28:57 +0700

    Update hello3.c

--- lab2/task1/hello3.c
+++ lab2/task1/hello3.c
@@ -1,12 +1,11 @@
-#include <unistd.h>
 #include <sys/syscall.h>
-#include <stdio.h>
+#include <unistd.h>
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 #define STD_OUT 1
 #define BUF_SIZE 16
-#define ERR_STATUS -1
+#define ERROR -1
 
 ssize_t mywrite(int fd, const void* buf, size_t count) {
         return syscall(SYS_write, fd, buf, count);
@@ -18,12 +17,13 @@ int main() {
 
     status = mywrite(STD_OUT, "Hello my world!\n", BUF_SIZE);
 
-    if (status == ERR_STATUS) {
-
-        printf("ERROR OCCURED");
+    if (status == ERROR)
+    {
         return EXIT_FAILURE;
-
     }
 
+    else
+    {
     return EXIT_SUCCESS;
+    }
 }
