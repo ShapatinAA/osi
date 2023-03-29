2b5bba6cb1a492dbe3034269baa98084576e500f
ShapatinAA <113165107+ShapatinAA@users.noreply.github.com>
2023-03-30 03:09:43 +0700

    Update hello.c

--- lab2/task2/hello.c
+++ lab2/task2/hello.c
@@ -1,19 +1,19 @@
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
-#define SYS_WRITE 1
+#define SYS_WRITE_CALL_NUMBER 1
 #define TRUE 1
 #define FALSE 0
-#define STD_OUTPUT 1
+#define STD_OUTPUT_DESCRIPTOR 1
 #define BUF_SIZE 14
 
 
 int main() {
 
-  register int     rdi          asm("rdi") = STD_OUTPUT;                //put stdout descript
-  register char*   rsi          asm("rsi") = "Hello, world!\n";         //put string
-  register int     rax          asm("rax") = SYS_WRITE;                 //put numer of syscall
-  register int     rdx          asm("rdx") = BUF_SIZE;                  //put buf size
+  register int     rdi          asm("rdi") = STD_OUTPUT_DESCRIPTOR;             //put stdout descript
+  register char*   rsi          asm("rsi") = "Hello, world!\n";                 //put string
+  register int     rax          asm("rax") = SYS_WRITE_CALL_NUMBER;             //put numer of syscall
+  register int     rdx          asm("rdx") = BUF_SIZE;                          //put buf size
 
   asm("syscall");                                                       //make syscall
 
