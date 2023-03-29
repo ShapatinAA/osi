6f4889334c77888c623ede867f16697f19b4bb2b
ShapatinAA <113165107+ShapatinAA@users.noreply.github.com>
2023-03-30 03:28:38 +0700

    Update hello2.c

--- lab2/task1/hello2.c
+++ lab2/task1/hello2.c
@@ -1,24 +1,23 @@
 #include <unistd.h>
-#include <stdio.h>
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 #define STD_OUT 1
 #define BUF_SIZE 13
-#define ERR_STATUS -1
+#define ERROR -1
 
 int main() {
 
-    sszie_t status;
+    ssize_t status;
 
     status = write(STD_OUT, "Hello world!\n", BUF_SIZE);
 
-    if (status == ERR_STATUS) {
-
-        printf("ERROR OCCURED");
+    if (status == ERROR)
+    {
         return EXIT_FAILURE;
-
     }
-
-    return EXIT_SUCCESS;
+    else
+    {
+        return EXIT_SUCCESS;
+    }
 }
