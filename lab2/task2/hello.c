16614fef282cafddd301341c768ff7489c00dcc9
ShapatinAA <113165107+ShapatinAA@users.noreply.github.com>
2023-03-30 03:07:35 +0700

    Update hello.c

--- lab2/task2/hello.c
+++ lab2/task2/hello.c
@@ -1,16 +1,45 @@
 
 #define EXIT_SUCCESS 0
-#define EXUT_FAILURE 1
+#define EXIT_FAILURE 1
+#define SYS_WRITE 1
+#define TRUE 1
+#define FALSE 0
+#define STD_OUTPUT 1
+#define BUF_SIZE 14
+
 
 int main() {
 
-  register int     arg1        asm("rdi") = 1;
-  register char*   arg2        asm("rsi") = "Hello, world!\n";
-  register int     syscall_no  asm("rax") = 1;
-  register int     arg3        asm("rdx") = 14;
+  register int     rdi          asm("rdi") = STD_OUTPUT;                //put stdout descript
+  register char*   rsi          asm("rsi") = "Hello, world!\n";         //put string
+  register int     rax          asm("rax") = SYS_WRITE;                 //put numer of syscall
+  register int     rdx          asm("rdx") = BUF_SIZE;                  //put buf size
 
-  asm("syscall");
+  asm("syscall");                                                       //make syscall
 
-  return EXIT_SUCCESS;
+
+
+  int sys_write_failure;
+
+  if (rax < 0)                                                          //checking status of the error
+  {
+    sys_write_failure = TRUE;
+  }
+  else
+  {
+    sys_write_failure = FALSE;
+  }
+
+
+
+  if (sys_write_failure == TRUE)
+  {
+    return EXIT_FAILURE;
+  }
+  else
+  {
+    return EXIT_SUCCESS;
+  }
+
 
 }
